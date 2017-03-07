bd9bdbffc9567f02033b91d1505cf0f84813ed87
hajnina <hajnina11@gmail.com>
2017-03-07 22:55:26 +0100

    Basic stuff and basic acceleration fov changes...
    next update: change fov manipulation to positional changes of the camera

---
+++ Assets/Auto.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Auto : MonoBehaviour {
+
+    public float vykon = 1000;
+    public float uhelRizeni = 30;
+    public string osaRizeni = "Horizontal";
+    public string osaPlynuABrzdy = "Vertical";
+
+    public WheelCollider lp, pp, lz, pz;
+    public Rigidbody auto;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        lp.steerAngle = pp.steerAngle = uhelRizeni * Input.GetAxis(osaRizeni);
+        if (Input.GetAxis(osaPlynuABrzdy) > 0)
+        {
+            Debug.Log(lz.motorTorque = pz.motorTorque = vykon * Input.GetAxis(osaPlynuABrzdy));
+            lz.brakeTorque = pz.brakeTorque = 0;
+        }
+        else
+        {
+            lz.motorTorque = pz.motorTorque = 0;
+            Debug.Log("b" +(lz.brakeTorque = pz.brakeTorque = -vykon * Input.GetAxis(osaPlynuABrzdy)).ToString());
+        }
+	}
+}
---
+++ Assets/CameraShake.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+
+    public Camera kamera;
+    public Rigidbody objekt;
+    public float sila = 1;
+    public float fov = 90;
+    Vector3 rychlost, lastPos;
+    float zrychleni;
+
+	// Use this for initialization
+	void Start () {
+        rychlost = objekt.velocity;
+        zrychleni = objekt.velocity.magnitude - rychlost.magnitude;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        zrychleni = objekt.velocity.magnitude - rychlost.magnitude;
+        rychlost = objekt.velocity;
+        kamera.fov = Mathf.Lerp(kamera.fov,fov + zrychleni * sila,Time.deltaTime);
+	}
+}
